8baed194f002a201b7c9ebdaeb69fb57c7e59284
Christian Theilemann <christian@aptoslabs.com>
2022-04-30 01:37:05 -0700

    [CICD] modernize+simplify docker build and enable caching

--- docker/build-common.sh
+++ docker/build-common.sh
@@ -3,11 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 set -e
 
-#either release or test
-if [ -z "$IMAGE_TARGETS" ]; then
-  IMAGE_TARGETS="all"
-fi
-
 # This is a common compilation scripts across different docker file
 # It unifies RUSFLAGS, compilation flags (like --release) and set of binary crates to compile in common docker layer
 
@@ -22,7 +17,7 @@ export CARGO_PROFILE_RELEASE_LTO=thin # override lto setting to turn on thin-LTO
 # TODO: consider using ${CARGO} once upstream issues are fixed.
 cargo x generate-workspace-hack --mode disable
 
-if [ "$IMAGE_TARGETS" = "release" ] || [ "$IMAGE_TARGETS" = "all" ]; then
+if [ "$IMAGE_TARGETS" = "release" ]; then
   # Build release binaries (TODO: use x to run this?)
   cargo build --release \
           -p aptos-genesis-tool \
@@ -40,15 +35,10 @@ if [ "$IMAGE_TARGETS" = "release" ] || [ "$IMAGE_TARGETS" = "all" ]; then
   # Build our core modules!
   cargo run --package framework -- --package aptos-framework --output current
 
-  # Build and overwrite the aptos-node binary with feature failpoints if $ENABLE_FAILPOINTS is configured
-  if [ "$ENABLE_FAILPOINTS" = "1" ]; then
-    echo "Building aptos-node with failpoints feature"
-    (cd aptos-node && cargo build --release --features failpoints "$@")
-  fi
 fi
 
 
-if [ "$IMAGE_TARGETS" = "test" ] || [ "$IMAGE_TARGETS" = "all"  ]; then
+if [ "$IMAGE_TARGETS" = "test" ]; then
   # These non-release binaries are built separately to avoid feature unification issues
   cargo build --release \
           -p aptos-faucet \
